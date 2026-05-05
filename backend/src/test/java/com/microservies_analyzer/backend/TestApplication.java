3241d44a67049e6f7275a21bb8e308e59e41d089
shubzz-001 <sdraje01@gmail.com>
2026-05-05 19:44:36 +0530

    Created backend folder for project

---
+++ backend/src/test/java/com/microservies_analyzer/backend/TestApplication.java
@@ -0,0 +1,11 @@
+package com.microservies_analyzer.backend;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
